e88f9a4c79a6eafea4d63c894a320d1ee81ed726
Durpat <durpatbalayar2@gmail.com>
2026-01-09 10:01:43 +0530

    employee task done

--- src/components/Daskboard/Employee/EmpTaskCard.jsx
+++ src/components/Daskboard/Employee/EmpTaskCard.jsx
@@ -1,98 +1,31 @@
 import React from "react";
+import AcceptTask from "../../TaskList/AcceptTask";
+import NewTask from "../../TaskList/NewTask";
+import CompleteTask from "../../TaskList/CompleteTask";
+import FailedTask from "../../TaskList/FailedTask";
 
-const EmpTaskCard = ({data}) => {
+const EmpTaskCard = ({ data }) => {
+  console.log(data.tasks);
   return (
     <div
       id="tasklist"
-      className=" flex items-center justify-start flex-nowrap gap-5 w-full   h-[50%] mt-10 overflow-x-auto"
+      className=" flex items-center justify-start flex-nowrap gap-5 w-full   h-[54%]  mt-10 pb-5  overflow-x-auto"
     >
-      <div className=" shrink-0 h-full w-75 bg-red-400 rounded-xl">
-        <div className="flex items-center justify-between px-3 py-4">
-          <h3 className="bg-red-600 px-5 py-1 rounded-lg text-base">High</h3>
-          <h4 className="text-sm">20 feb 2024</h4>
-        </div>
-        <h1 className="text-2xl font-bold px-3 mt-2">Make a Youtube Video</h1>
+      {data.tasks.map((elem) => {
+        if (elem.active) {
+          return <AcceptTask />;
+        }
 
-        <p className="text-xl px-3 mt-5">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit ab
-          quis sapiente veniam molestias est?
-        </p>
-      </div>
-
-      <div className=" shrink-0 h-full w-75 bg-green-400 rounded-xl">
-        <div className="flex items-center justify-between px-3 py-4">
-          <h3 className="bg-red-600 px-5 py-1 rounded-lg text-base">High</h3>
-          <h4 className="text-sm">20 feb 2024</h4>
-        </div>
-        <h1 className="text-2xl font-bold px-3 mt-2">Make a Youtube Video</h1>
-
-        <p className="text-xl px-3 mt-5">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit ab
-          quis sapiente veniam molestias est?
-        </p>
-      </div>
-
-      <div className=" shrink-0 h-full w-75 bg-gray-400 rounded-xl">
-        <div className="flex items-center justify-between px-3 py-4">
-          <h3 className="bg-red-600 px-5 py-1 rounded-lg text-base">High</h3>
-          <h4 className="text-sm">20 feb 2024</h4>
-        </div>
-        <h1 className="text-2xl font-bold px-3 mt-2">Make a Youtube Video</h1>
-
-        <p className="text-xl px-3 mt-5">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit ab
-          quis sapiente veniam molestias est?
-        </p>
-      </div>
-      <div className=" shrink-0 h-full w-75 bg-blue-400 rounded-xl">
-        <div className="flex items-center justify-between px-3 py-4">
-          <h3 className="bg-red-600 px-5 py-1 rounded-lg text-base">High</h3>
-          <h4 className="text-sm">20 feb 2024</h4>
-        </div>
-        <h1 className="text-2xl font-bold px-3 mt-2">Make a Youtube Video</h1>
-
-        <p className="text-xl px-3 mt-5">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit ab
-          quis sapiente veniam molestias est?
-        </p>
-      </div>
-      <div className=" shrink-0 h-full w-75 bg-orange-400 rounded-xl">
-        <div className="flex items-center justify-between px-3 py-4">
-          <h3 className="bg-red-600 px-5 py-1 rounded-lg text-base">High</h3>
-          <h4 className="text-sm">20 feb 2024</h4>
-        </div>
-        <h1 className="text-2xl font-bold px-3 mt-2">Make a Youtube Video</h1>
-
-        <p className="text-xl px-3 mt-5">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit ab
-          quis sapiente veniam molestias est?
-        </p>
-      </div>
-      <div className=" shrink-0 h-full w-75 bg-emerald-400 rounded-xl">
-        <div className="flex items-center justify-between px-3 py-4">
-          <h3 className="bg-red-600 px-5 py-1 rounded-lg text-base">High</h3>
-          <h4 className="text-sm">20 feb 2024</h4>
-        </div>
-        <h1 className="text-2xl font-bold px-3 mt-2">Make a Youtube Video</h1>
-
-        <p className="text-xl px-3 mt-5">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit ab
-          quis sapiente veniam molestias est?
-        </p>
-      </div>
-      <div className=" shrink-0 h-full w-75 bg-red-400 rounded-xl">
-        <div className="flex items-center justify-between px-3 py-4">
-          <h3 className="bg-red-600 px-5 py-1 rounded-lg text-base">High</h3>
-          <h4 className="text-sm">20 feb 2024</h4>
-        </div>
-        <h1 className="text-2xl font-bold px-3 mt-2">Make a Youtube Video</h1>
-
-        <p className="text-xl px-3 mt-5">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit ab
-          quis sapiente veniam molestias est?
-        </p>
-      </div>
-
+        if (elem.newTask) {
+          return <NewTask />;
+        }
+        if (elem.completed) {
+          return <CompleteTask />;
+        }
+        if (elem.failed) {
+          return <FailedTask />;
+        }
+      })}
     </div>
   );
 };
--- src/components/TaskList/AcceptTask.jsx
+++ src/components/TaskList/AcceptTask.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import React from "react";
 
 const AcceptTask = () => {
   return (
-    <div className=" shrink-0 h-full w-75 bg-red-400 rounded-xl">
+    <div className=" shrink-0 h-full w-95 bg-red-400 rounded-xl">
       <div className="flex items-center justify-between px-3 py-4">
         <h3 className="bg-red-600 px-5 py-1 rounded-lg text-base">High</h3>
         <h4 className="text-sm">20 feb 2024</h4>
@@ -13,8 +13,17 @@ const AcceptTask = () => {
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit ab quis
         sapiente veniam molestias est?
       </p>
+
+      <div className="flex justify-between mt-4 px-2">
+        <button className="bg-green-500 py-1 px-2 text-sm rounded  ">
+          Mark as Completed
+        </button>
+        <button className="bg-red-500 py-1 px-2 text-sm rounded">
+          Mark as Failed
+        </button>
+      </div>
     </div>
   );
-}
+};
 
-export default AcceptTask
+export default AcceptTask;
