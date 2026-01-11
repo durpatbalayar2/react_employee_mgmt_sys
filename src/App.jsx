7bfef41466da3e2d853261c56575fdf1324afc4f
Durpat <durpatbalayar2@gmail.com>
2026-01-11 21:58:06 +0530

    fix page reloading

--- src/App.jsx
+++ src/App.jsx
@@ -9,62 +9,60 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 
 const App = () => {
-  const [user, setUser] = useState(null);
-  const [loggedInUserData, setLoggedInUserData] = useState(null);
+
+  const [user, setUser] = useState(() => {
+    const loggedInUser = localStorage.getItem("loggedInUser");
+    return loggedInUser ? JSON.parse(loggedInUser).role : null;
+  });
+
+  const [loggedInUserData, setLoggedInUserData] = useState(() => {
+    const loggedInUser = localStorage.getItem("loggedInUser");
+    return loggedInUser ? JSON.parse(loggedInUser).data || null : null;
+  });
+
   const [authData] = useContext(AuthContext);
 
+  useEffect(() => {
+    const loggedInUser = localStorage.getItem("loggedInUser");
 
+    if (loggedInUser) {
+      const userData = JSON.parse(loggedInUser);
+      setUser(userData.role);
+      setLoggedInUserData(userData.data);
+    }
+  }, []);
 
-  useEffect(()=>{
-
-    const loggedInUser = localStorage.getItem('loggedInUser')
-
-    if(loggedInUser){
-      const userData = JSON.parse(loggedInUser)
-      setUser(userData.role)
-
-      setLoggedInUserData(userData.data)
+  const handleLogin = (email, password) => {
+    if (email === "admin@me.com" && password === "123") {
+      setUser("admin");
+      localStorage.setItem("loggedInUser", JSON.stringify({ role: "admin" }));
+      return "admin";
     }
 
-
-  },[])
-
-
-
-const handleLogin = (email, password) => {
-  if (email === "admin@me.com" && password === "123") {
-    setUser("admin");
-    localStorage.setItem("loggedInUser", JSON.stringify({ role: "admin" }));
-    return "admin";
-  }
-
-  if (authData) {
-    const employee = authData.employees.find(
-      (e) => email === e.email && password === e.password
-    );
-
-    if (employee) {
-      setUser("employee");
-      setLoggedInUserData(employee);
-      localStorage.setItem(
-        "loggedInUser",
-        JSON.stringify({ role: "employee", data: employee })
+    if (authData) {
+      const employee = authData.employees.find(
+        (e) => email === e.email && password === e.password
       );
-      return "employee";
+
+      if (employee) {
+        setUser("employee");
+        setLoggedInUserData(employee);
+        localStorage.setItem(
+          "loggedInUser",
+          JSON.stringify({ role: "employee", data: employee })
+        );
+        return "employee";
+      }
     }
-  }
 
-  alert("Invalid Credentials");
-  return null;
-};
-
+    alert("Invalid Credentials");
+    return null;
+  };
 
   return (
-
-   
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/about"element={<About/>}/>
+      <Route path="/about" element={<About />} />
       <Route path="/login" element={<Login handleLogin={handleLogin} />} />
 
       <Route
